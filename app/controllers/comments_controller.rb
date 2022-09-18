2e5d556906adfad7dce653a9134878ba3fba8dd8
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-09-18 10:06:25 +0000

    [Finish] 6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,29 @@
 class CommentsController < ApplicationController
-  
+
   def create
     @book = Book.find(params[:book_id])
-    comment = Comment.new(comment_params)
-    comment.user_id = current_user.id
-    comment.book_id =  @book.id
-    if comment.save
-      redirect_back(fallback_location: book_path(@book.id))
-    else
-      render :show
-    end
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id =  @book.id
+    @users = User.all
+    #if @comment.save
+    #  redirect_back(fallback_location: book_path(@book.id))
+    #else
+    #  render "books/show"
+    #end
+    @comment.save
   end
-  
+
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @users = User.all
     comment = Comment.find(params[:id])
     comment.destroy
-    redirect_back(fallback_location: book_path(book.id))
+    #redirect_back(fallback_location: book_path(book.id))
   end
-  
+
   def comment_params
     params.require(:comment).permit(:comment_body)
   end
-  
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :book
   belongs_to :user
+  
+  validates :comment_body, presence: true, length:{maximum:200}
 end
